1bd9eaaeb84e19359b5b93102fe200b0dfa7acd3
Pranav Mamgain <pranavmamgain6@gmail.com>
2023-09-09 15:26:10 +0530

    updated localstorage

--- linkshare.js
+++ linkshare.js
@@ -1,8 +1,9 @@
 debugger;
 window.onload = function () {
     pageShow();
-    debugger;
 };
+// Event listener for storing Data
+document.querySelector(".save-btn").addEventListener('click', storeData);
 
 function pageShow() {
     debugger;
@@ -11,13 +12,19 @@ function pageShow() {
     const linksBtn = document.querySelector(".link-btn");
     const profileBtn = document.querySelector(".profile-btn");
 
+
     linksBtn.addEventListener('click', () => {
         linksPage.style.display = "block";
         profilePage.style.display = "none";
+        linksBtn.classList.add("active-color");
+        profileBtn.classList.remove("active-color");
+
     });
     profileBtn.addEventListener('click', () => {
         profilePage.style.display = "block";
         linksPage.style.display = "none";
+        profileBtn.classList.add("active-color");
+        linksBtn.classList.remove("active-color");
     });
 }
 
@@ -25,17 +32,53 @@ function addLinks() {
     let addLinkbtn = document.querySelector(".link-add-btn");
     let linkDiv = document.querySelector(".new-link");
 
-    addLinkbtn.addEventListener('click', ()=>{
+    addLinkbtn.addEventListener('click', () => {
 
     });
 }
 
-function storeData() {
+    
+
+function storeLinkData() {
+    debugger;
     let platformList = document.getElementById("platform");
     let inputLink = document.getElementById("link-input");
+    
+
+    const dataLink = {
+        platform: platformList.value,
+        link: inputLink.value
+    };
+
+    let tempArray = JSON.parse(localStorage.getItem("Data"));
+
+    let finalArray = tempArray ? tempArray : [];
+    finalArray.push(dataLink);
+
+    localStorage.setItem("Data", JSON.stringify(finalArray));
+}
+
+function storePersonalData() {
     let fName = document.getElementById("firstname");
     let lName = document.getElementById("lastname");
     let email = document.getElementById("mail");
 
-    
+    const dataPersonal = {
+        first_name : fName.value,
+        last_name : lName.value,
+        mail : email.value
+    };
+}
+
+function checkStorage() {
+    let arrayCheck = JSON.parse(localStorage.getItem("Data"));
+    let updateArray = arrayCheck ? arrayCheck : [];
+
+    if (updateArray.length > 0) {
+        showData(updateArray);
+    }
+}
+
+function showData(storageData){
+
 }
