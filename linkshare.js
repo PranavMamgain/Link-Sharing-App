2274995f26d5f359964313ebe5e125ab5b5b04a2
Pranav Mamgain <pranavmamgain6@gmail.com>
2023-09-13 17:48:35 +0530

    Updated Js file
    function added:
    1. create new link
    2. remove link
    3. update in local storage

--- linkshare.js
+++ linkshare.js
@@ -2,9 +2,22 @@ debugger;
 window.onload = function () {
     pageShow();
     // checkStorage();
+    showLinkData();
 };
-// Event listener for storing Data
+// Event listener 
 document.querySelector(".save-btn").addEventListener('click', storeLinkData);
+document.querySelector(".save-profile-btn").addEventListener('click', storePersonalData);
+document.querySelector(".link-add-btn").addEventListener('click', createLink);
+document.querySelector(".links-list").addEventListener('click', (event) => {
+    if (event.target && event.target.classList.contains("remove-btn")) {
+        const divLink = event.target.closest(".new-link");
+        if (divLink) {
+            const divIndex = Array.from(divLink.parentElement.children).indexOf(divLink);
+            divLink.remove();
+            removeFromLocalStorage(divIndex);
+        }
+    }
+});
 
 function pageShow() {
     debugger;
@@ -29,35 +42,24 @@ function pageShow() {
     });
 }
 
-function addLinks() {
-    let addLinkbtn = document.querySelector(".link-add-btn");
-    let linkDiv = document.querySelector(".new-link");
-
-    addLinkbtn.addEventListener('click', () => {
-
-    });
-}
-
-    
-
 function storeLinkData() {
     debugger;
-    let currentIdCounter = parseInt(localStorage.getItem('currentIdCounter')) || 1;
-    let platformList = document.getElementById("platform");
-    let inputLink = document.getElementById("link-input");
-    
-
-    const dataLink = {
-        platform: platformList.value,
-        link: inputLink.value
-    };
-
-    let tempArray = JSON.parse(localStorage.getItem("Data"));
-
+    let tempArray = JSON.parse(localStorage.getItem("LinkData"));
     let finalArray = tempArray ? tempArray : [];
-    finalArray.push(dataLink);
 
-    localStorage.setItem("Data", JSON.stringify(finalArray));
+    let linkElements = document.querySelectorAll(".new-link");
+
+    linkElements.forEach(element => {
+        let platformList = element.querySelector("#platform").value;
+        let inputLink = element.querySelector("#link-input").value;
+
+        const dataLink = {
+            platform: platformList,
+            link: inputLink
+        };
+        finalArray.push(dataLink);
+    });
+    localStorage.setItem("LinkData", JSON.stringify(finalArray));
 }
 
 function storePersonalData() {
@@ -66,14 +68,16 @@ function storePersonalData() {
     let email = document.getElementById("mail");
 
     const dataPersonal = {
-        first_name : fName.value,
-        last_name : lName.value,
-        mail : email.value
+        first_name: fName.value,
+        last_name: lName.value,
+        mail_id: email.value    
     };
+
+    localStorage.setItem("PersonalData", JSON.stringify(dataPersonal));
 }
 
 function checkStorage() {
-    let arrayCheck = JSON.parse(localStorage.getItem("Data"));
+    let arrayCheck = JSON.parse(localStorage.getItem("LinkData"));
     let updateArray = arrayCheck ? arrayCheck : [];
 
     if (updateArray.length > 0) {
@@ -81,6 +85,54 @@ function checkStorage() {
     }
 }
 
-function showData(storageData){
+function showLinkData() {
+    debugger;
+    const storedData = JSON.parse(localStorage.getItem("LinkData"));
+    storedData.forEach((boxData) => {
+        createLink();
+        const boxElements = document.querySelectorAll('.new-link');
+        const lastBox = boxElements[boxElements.length - 1];
+        // lastBox.querySelector('.header-link span').textContent = boxData.title;
+        lastBox.querySelector('#platform').value = boxData.platform;
+        lastBox.querySelector('#link-input').value = boxData.link;
+    });
+}
 
+function createLink() {
+    let showList = document.querySelector(".links-list");
+    let newLinkElement = document.createElement("div");
+    newLinkElement.setAttribute("class", "new-link");
+
+    newLinkElement.innerHTML = `
+    <div class="header-link">
+                                    <span class="link-no">Link # <li></li></span>
+                                    <button class="remove-btn">Remove</button>
+                                </div>
+                                <div class="input-platform">
+                                    <p>Platform</p>
+                                    <select name="platform" id="platform">
+                                        <option value="Github"><span class="github-img"></span>Github</option>
+                                        <option value="Twitter"><span class="Twitter-img"></span>Twitter</option>
+                                        <option value="LinkedIn"><span class="LinkedIn-img"></span>LinkedIn</option>
+                                        <option value="YouTube"><span class="YouTube-img"></span>YouTube</option>
+                                        <option value="Facebook"><span class="Facebook-img"></span>Facebook</option>
+                                    </select>
+                                </div>
+                                <div class="input-link">
+                                    <p>Enter Link</p>
+                                    <input type="url" name="url" id="link-input" placeholder="https://example.com" required>
+                                </div>
+    `;
+    showList.appendChild(newLinkElement);
+
+    let mockupList = document.querySelector(".mockup");
+    let newDivElement = document.createElement("div");
+    newDivElement.classList.add("phone-div-link");
+    mockupList.appendChild(newDivElement);
+}
+
+function removeFromLocalStorage(index){
+    const storedData = JSON.parse(localStorage.getItem("LinkData"));
+    storedData.splice(index,1);
+    localStorage.setItem("LinkData", JSON.stringify(storedData));
 }
